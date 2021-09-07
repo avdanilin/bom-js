880870191f6648cc4a889632724a355303d9e3eb
Андрей Данилин <danilin@artw.ru>
2021-09-07 21:34:34 +0300

    added removed cookie accross delegation and function replace exist cookie

--- src/cookie.js
+++ src/cookie.js
@@ -9,14 +9,14 @@
  * На странице должна быть форма для добавления новой cookie:
  * - имя
  * - значение
- * - добавить (при нажатии на кнопку, в браузер и таблицу добавляется новая cookie с указанным именем и значением)
+ * - добавить (при нажатии на кнопку, в браузерe и таблиц добавляется новая cookie с указанным именем и значением)
  *
  * Если добавляется cookie с именем уже существующией cookie, то ее значение в браузере и таблице должно быть обновлено
  *
  * На странице должно быть текстовое поле для фильтрации cookie
  * В таблице должны быть только те cookie, в имени или значении которых есть введенное значение
  * Если в поле фильтра пусто, то должны выводиться все доступные cookie
- * Если дабавляемая cookie не соответсвуте фильтру, то она должна быть добавлена только в браузер, но не в таблицу
+ * Если да бавляемая cookie не соответсвуте фильтру, то она должна быть добавлена только в браузер, но не в таблицу
  * Если добавляется cookie, с именем уже существующией cookie и ее новое значение не соответствует фильтру,
  * то ее значение должно быть обновлено в браузере, а из таблицы cookie должна быть удалена
  *
@@ -35,17 +35,72 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     let homeworkContainer = document.querySelector('#homework-container');
-    console.log(homeworkContainer)
     let filterNameInput = homeworkContainer.querySelector('#filter-name-input');
     let addNameInput = homeworkContainer.querySelector('#add-name-input');
     let addValueInput = homeworkContainer.querySelector('#add-value-input');
     let addButton = homeworkContainer.querySelector('#add-button');
     let listTable = homeworkContainer.querySelector('#list-table tbody');
+
+    const removeButtonCookie = document.querySelector('.cookie_remove');
+
     filterNameInput.addEventListener('keyup', function () {
     });
 
-    addButton.addEventListener('click', () => {
-    });
+    document.addEventListener('click', removeCookie);
+    addButton.addEventListener('click', addCookie);
 
+
+    function addCookie() {
+        const getCookieName = addNameInput.value;
+        const getCookieValue = addValueInput.value;
+
+        const tr = document.createElement('tr');
+        const removeButton = document.createElement('button');
+
+        removeButton.classList.add('cookie_remove');
+        removeButton.textContent = 'Удалить куку';
+
+        document.cookie = `${getCookieName}=${getCookieValue};`;
+
+        const exist = existCookie();
+
+        if (!exist) {
+            tr.insertAdjacentHTML("afterbegin", `<td class="name">${getCookieName}</td><td>${getCookieValue}</td><td>${removeButton.outerHTML}</td>`);
+
+            listTable.append(tr);
+        }
+    }
+
+
+    function removeCookie(e) {
+        if (e.target.classList.contains('cookie_remove')) {
+            const targetParent = e.target.parentElement.parentElement;
+
+            for (let i = 0; i < targetParent.children.length; i++) {
+                const cookieName = targetParent.children[i];
+
+                if (cookieName.classList.contains('name')) {
+
+                    const removedCookie = cookieName.textContent;
+                    document.cookie = `${removedCookie}=;expires=${new Date(0)}`;
+                }
+            }
+
+            targetParent.remove();
+        }
+    }
+
+    function existCookie() {
+        let flag = false;
+        const currentCookies = listTable.querySelectorAll('.name');
+
+        currentCookies.forEach(currentCookie => {
+            if (currentCookie.textContent === addNameInput.value) {
+                flag = true;
+            }
+        });
+
+        return flag;
+    }
 })
 
